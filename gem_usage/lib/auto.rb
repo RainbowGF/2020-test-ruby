c006c6b881afe888625a9c1eb3b3264834f96480
Vladislav <grifon7676@gmail.com>
2020-04-06 11:36:55 +0300

    updated car lab

---
+++ gem_usage/lib/auto.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Auto
+  attr_reader :mark
+  attr_reader :model
+  attr_reader :year
+  attr_reader :benz
+
+  def initialize(mark, model,year,benz)
+      @mark = mark
+      @model = model
+      @year = year
+      @benz = benz
+  end
+
+  def to_s
+      "Марка: #{mark}, Модель: #{model}, Год: #{year}, Бензин #{benz}"
+  end
+end
---
+++ gem_usage/lib/fleet.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class Fleet
+
+  attr_reader :cars
+  
+  def initialize
+    @cars = []
+  end
+
+  def add(new_car)
+    @cars.push(new_car)
+  end
+
+  def load_from_file(file_name)
+    require 'json'
+    json_data = File.read(file_name)
+    ruby_objects = JSON.parse(json_data)
+    ruby_objects.each do |car|
+      add(Auto.new(car['mark'], car['model'], car['year'], car['consumption']))
+    end
+  end
+
+  def average_consumption
+    whoal_benz = 0
+    cars.each do |car|
+      whoal_benz += car.benz
+    end
+    whoal_benz / cars.size
+    end
+
+  def number_by_brand(brand)
+    whoal_cars_by_brand = 0
+    cars.each do |car|
+      whoal_cars_by_brand += 1 if car.mark.downcase == brand.downcase
+    end
+    whoal_cars_by_brand
+  end
+
+  def number_by_model(model)
+    whoal_cars_by_model = 0
+    cars.each do |car|
+      whoal_cars_by_model += 1 if car.model.downcase == model.downcase
+    end
+    whoal_cars_by_model
+  end
+
+  def consumption_by_brand(brand)
+    whoal_cars_by_brand = 0
+    whoal_benz = 0
+    cars.each do |car|
+      if car.mark.downcase == brand.downcase
+        whoal_cars_by_brand += 1
+        whoal_benz += car.benz
+      end
+    end
+    return 0 if whoal_cars_by_brand <= 0
+
+    whoal_benz / whoal_cars_by_brand
+  end
+end
